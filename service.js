82f330679b61e95fde8541ab270ec04685ed958b
stagiaire <stagiaire@cpg-453069.cepegra2008.lan>
2019-11-12 11:52:34 +0100

    Workbox et cache

--- service.js
+++ service.js
@@ -13,3 +13,27 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", () => {
     // met en écoute le service
 })
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+    workbox.precaching.precacheAndRoute([
+        {
+            "url" : "index.html"
+        },
+        {
+            "url" : "style.css"
+        },
+        {
+            "url" : "manifest.json"
+        },
+        {
+            "url" : "icon-96-96.png"
+        },
+        {
+            "url" : "favicon.ico"
+        }
+    ])
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
